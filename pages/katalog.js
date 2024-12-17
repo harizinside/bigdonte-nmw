56c7027af4b115050d253a5e40d6d1f7cbb9eb63
flyyy26 <rfchannel0@gmail.com>
2024-12-17 11:50:57 +0700

    Repaired Katalog

--- pages/katalog.js
+++ pages/katalog.js
@@ -1,85 +1,129 @@
-import banner from "@/styles/Banner.module.css";
-import styles from "@/styles/Kebijakan.module.css";
+import banner from "@/styles/Banner.module.css"
+import styles from "@/styles/Catalog.module.css"
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import loadingStyles from "@/styles/Loading.module.css";
 
 export default function KebijakanPrivasi() {
-  const [kebijakans, setKebijakans] = useState([]); // Default sebagai array
+  const [catalogs, setCatalogs] = useState([]);
+  const [loading, setLoading] = useState(true);
   const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
   const mainUrl = process.env.NEXT_PUBLIC_API_MAIN_URL;
   const storageUrl = process.env.NEXT_PUBLIC_API_STORAGE_URL;
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch(`${baseUrl}/kebijakan`);
-        const data = await response.json();
-        if (Array.isArray(data)) { // Pastikan data adalah array
-          setKebijakans(data); // Setel data array kebijakan
-        } else {
-          console.error("Invalid response data format:", data);
-        }
-      } catch (error) {
-        console.error("Error fetching kebijakan:", error);
-      }
+        const cachedData = localStorage.getItem('promoCache');
+        const cacheExpiry = localStorage.getItem('promoCacheExpiry');
+        const now = new Date().getTime();
+
+        try {
+            const response = await fetch(`${baseUrl}/catalog`);
+            const data = await response.json();
+
+            if (data && data.data) {
+                if (cachedData && cacheExpiry && now < parseInt(cacheExpiry)) {
+                    const parsedCache = JSON.parse(cachedData);
+                    
+                    if (JSON.stringify(parsedCache) !== JSON.stringify(data.data)) {
+                        console.log('Data updated from API');
+                        setCatalogs(data.data);
+                        localStorage.setItem('promoCache', JSON.stringify(data.data));
+                        localStorage.setItem('promoCacheExpiry', (now + 6 * 60 * 60 * 1000).toString());
+                    } else {
+                        console.log('Loaded from cache');
+                        setCatalogs(parsedCache);
+                    }
+                } else {
+                    console.log('Fetched from API');
+                    setCatalogs(data.data);
+                    localStorage.setItem('promoCache', JSON.stringify(data.data));
+                    localStorage.setItem('promoCacheExpiry', (now + 6 * 60 * 60 * 1000).toString());
+                }
+            } else {
+                console.error('Invalid response data format:', data);
+            }
+        } catch (error) {
+            console.error('Error fetching banners:', error);
+            if (cachedData) {
+                setCatalogs(JSON.parse(cachedData));
+                console.log('Loaded from cache after API error');
+            }
+        } finally {
+            setLoading(false);
+        } 
     };
 
     fetchData();
   }, [baseUrl]);
 
-  const schemaData = {
-      "@context": "https://schema.org",
-      "@type": "WebPage",
-      name: `Kebijakan Privasi - NMW Aesthetic Clinic`,
-      description: `Temukan jawaban atas pertanyaan umum tentang layanan, perawatan, konsultasi, dan prosedur medis di NMW Aesthetic Clinic. Dapatkan informasi lengkap untuk perawatan kecantikan dan kesehatan kulit Anda.`,
-      url: `${mainUrl}/kebijakan-privasi`,
-      publisher: {
-      "@type": "Organization",
-      name: "NMW Aesthetic Clinic",
-      logo: {
-          "@type": "ImageObject",
-          url: `${mainUrl}/images/kebijakan-privasi.png`
-      }
-      },
-      mainEntityOfPage: {
-      "@type": "WebPage",
-      "@id": `${mainUrl}/kebijakan-privasi`
-      },
-      breadcrumb: {
-          "@type": "BreadcrumbList",
-          itemListElement: [
-              {
-              "@type": "ListItem",
-                  position: 1,
-                  name: "Home",
-                  item: `${mainUrl}`
-              },
-              {
-              "@type": "ListItem",
-              position: 2,
-                  name: "Kebijakan Privasi",
-                  item: `${mainUrl}/kebijakan-privasi`
-              }
-          ]
-      }
-  };
+  if (loading) {
+        return (
+            <>
+                <div className={loadingStyles.box}>
+                    <div className={loadingStyles.content}>
+                        <img src="../images/logo.svg"/>
+                        <span>Loading</span>
+                    </div>
+                </div>
+            </>
+        );
+    }
+
+    const schemaData = {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        name: `Katalog - NMW Aesthetic Clinic`,
+        description: `Lihat dan download katalog NMW Aesthetic Clinic`,
+        url: `${mainUrl}/catalog`,
+        publisher: {
+        "@type": "Organization",
+        name: "NMW Aesthetic Clinic",
+        logo: {
+            "@type": "ImageObject",
+            url: `${mainUrl}/images/catalogue-banner.png`
+        }
+        },
+        mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": `${mainUrl}/catalog`
+        },
+        breadcrumb: {
+            "@type": "BreadcrumbList",
+            itemListElement: [
+                {
+                "@type": "ListItem",
+                    position: 1,
+                    name: "Home",
+                    item: `${mainUrl}`
+                },
+                {
+                "@type": "ListItem",
+                position: 2,
+                    name: "Katalog",
+                    item: `${mainUrl}/catalog`
+                }
+            ]
+        }
+    };
 
   return (
     <>
         <Head>
           <title>Katalog | NMW Aesthetic Clinic</title>
-          <meta name="description" content="Berikut kebijakan privasi untuk melakukan akses website NMW Aesthetic Clinic" />
-          <meta name="keywords" content="kebijakan privasi, kebijakan, privasi, kebijakan privasi nmw clinic, nmw clinic" />
+          <meta name="description" content="Lihat dan download katalog NMW Aesthetic Clinic" />
+          <meta name="keywords" content="katalog NMW Clinic, produk kecantikan, perawatan kulit, skincare terbaik, produk perawatan wajah, kosmetik profesional, perawatan tubuh, produk anti-aging, serum wajah, krim pelembab, tabir surya, masker wajah, produk perawatan rambut, suplemen kecantikan, kosmetik medis, krim malam, perawatan kulit sensitif, produk klinik kecantikan, skincare rekomendasi dokter, kosmetik terpercaya, produk kesehatan kulit, perawatan kecantikan lengkap, solusi kecantikan, katalog produk NMW" />
 
           <meta property="og:title" content="Katalog NMW Aesthetic Clinic"  />
-          <meta property="og:description" content="Berikut kebijakan privasi untuk melakukan akses website NMW Aesthetic Clinic" />
+          <meta property="og:description" content="Lihat dan download katalog NMW Aesthetic Clinic" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content={`${mainUrl}/katalog`} />
           <meta property="og:image" content={`${mainUrl}/images/catalogue-banner.png`} />
 
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Katalog NMW Aesthetic Clinic" />
-          <meta name="twitter:description" content="Berikut kebijakan privasi untuk melakukan akses website NMW Aesthetic Clinic" />
+          <meta name="twitter:description" content="Lihat dan download katalog NMW Aesthetic Clinic" />
           <meta name="twitter:image" content={`${mainUrl}/images/catalogue-banner.png`} />
 
           <link rel="canonical" href={`${mainUrl}/katalog`} />
@@ -96,19 +140,47 @@ export default function KebijakanPrivasi() {
       </div>
       <div className={styles.container}>
         <div className={`${styles.heading_section}`}>
-          <h1>
-            <font>Kebijakan</font> Privasi
-          </h1>
+            <h1><font>Katalog</font> Harga</h1>
         </div>
-        <div className={styles.kebijakan_layout}>
-          {kebijakans.map((item, index) => (
-            <div
-              key={index}
-              dangerouslySetInnerHTML={{ __html: item.kebijakan }} // Render HTML dari API
-            />
-          ))}
+            <div className={styles.box_galeri_layout}>
+                {catalogs.map(catalog => (
+                    <div className={styles.box_galeri} key={catalog.id}>
+                        <div className={styles.box_galeri_image}>
+                            <img src={`${storageUrl}/${catalog.image}`} alt={catalog.title}/>
+                        </div>
+                        <div className={styles.box_galeri_content}>
+                            <div className={styles.box_galeri_heading}>
+                                <h1>{catalog.title}</h1>
+                            </div>
+                            <div className={styles.box_galeri_text}>
+                                <p>Terakhir Diperbaharui</p>
+                                <p>{new Date(catalog.date).toLocaleDateString('id-ID', { day: '2-digit', month: 'long', year: 'numeric' })}</p>
+                            </div>
+                        </div>
+                        <div className={styles.box_galeri_button}>
+                            <Link href={`${storageUrl}/${catalog.document}`} target="blank_"><button>Unduh</button></Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
+        <div className={styles.section_4}>
+            <div className={styles.heading_section_4}>
+                <div className={`${styles.heading_section} ${styles.heading_section_start}`}>
+                    <h1><font>Dokter </font>Kami</h1>
+                </div>
+            </div>
+            <div className={styles.section_4_box}>
+            <img src="images/nmw_dokter.png" alt="Dokter-dokter NMW Aesthetic Clinic" className={styles.our_dokter} />
+            <img src="images/blink_orange.svg" alt="Blink Material" className={styles.section_icon_5} />
+            <img src="images/blink_grey.svg" alt="Blink Material" className={styles.section_icon_6} />
+            <div className={styles.section_4_content}>
+                <p>Dokter NMW Aesthetic Clinic adalah dokter terpilih, terlatih secara profesional, dan terpercaya untuk melakukanbedah plastik, dermatologi, spesialis kulit dan kelamin dan perawatan kulit ekstetika.</p>
+                <p>Dokter kami telah menjalani pelatihan ekstensif dan memiliki keahlian untuk memberikan hasil luar biasa sekaligus memastikan keselamatan pasien.</p>
+                <Link href={'/dokter-kami'}><button>Lihat Lebih Lanjut</button></Link>
+            </div>
+            </div>
+        </div>
     </>
   );
 }
