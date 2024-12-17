6b6f6888c62b58d75e67e6491afcf14c297762bb
flyyy26 <rfchannel0@gmail.com>
2024-12-17 11:00:50 +0700

    UseEffect

--- pages/branches.js
+++ pages/branches.js
@@ -5,9 +5,130 @@ import { useState, useEffect } from "react";
 
 export default function KebijakanPrivasi() {
   const [kebijakans, setKebijakans] = useState([]); // Default sebagai array
+  const [branchs, setBranchs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [settings, setSettings] = useState([]);
   const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
   const mainUrl = process.env.NEXT_PUBLIC_API_MAIN_URL;
   const storageUrl = process.env.NEXT_PUBLIC_API_STORAGE_URL;
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const cachedSetting = localStorage.getItem('settingCache');
+        const cachedSettingExpired = localStorage.getItem('settingCacheExpired');
+        const now = new Date().getTime();
+
+        // Cek apakah cache valid
+        if (cachedSetting && cachedSettingExpired && now < parseInt(cachedSettingExpired)) {
+            setSettings(JSON.parse(cachedSetting));
+            
+            // Lakukan pengecekan data API untuk pembaruan data
+            try {
+                const response = await fetch(`${baseUrl}/setting`);
+                const data = await response.json();
+
+                if (data && data.social_media) {
+                    const cachedData = JSON.parse(cachedSetting);
+                    
+                    // Bandingkan data baru dengan cache
+                    if (JSON.stringify(data) !== JSON.stringify(cachedData)) {
+                        setSettings(data);
+                        localStorage.setItem('settingCache', JSON.stringify(data));
+                        localStorage.setItem('settingCacheExpired', (now + 86400000).toString());
+                        console.log('Cache updated after API check');
+                    } else {
+                        console.log('No changes detected in API data');
+                    }
+                } else {
+                    console.error('Invalid API response:', data);
+                }
+            } catch (error) {
+                console.error('Error checking API for updates:', error);
+            }
+            return;
+        }
+
+        // Fetch data jika tidak ada cache atau cache sudah kadaluarsa
+        try {
+            const response = await fetch(`${baseUrl}/setting`);
+            const data = await response.json();
+
+            if (data && data.social_media) {
+                setSettings(data);
+                localStorage.setItem('settingCache', JSON.stringify(data));
+                localStorage.setItem('settingCacheExpired', (now + 86400000).toString());
+                console.log('Fetched and cached from API');
+            } else {
+                console.error('Invalid API response:', data);
+            }
+        } catch (error) {
+            console.error('Error fetching settings:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchData();
+  }, [baseUrl]);
+
+useEffect(() => {
+    const fetchData = async () => {
+        const cachedData = localStorage.getItem('promoCache');
+        const cacheExpiry = localStorage.getItem('promoCacheExpiry');
+        const now = new Date().getTime();
+
+        try {
+            const response = await fetch(`${baseUrl}/branch`);
+            const data = await response.json();
+
+            if (data && data.data) {
+                if (cachedData && cacheExpiry && now < parseInt(cacheExpiry)) {
+                    const parsedCache = JSON.parse(cachedData);
+                    
+                    if (JSON.stringify(parsedCache) !== JSON.stringify(data.data)) {
+                        console.log('Data updated from API');
+                        setBranchs(data.data);
+                        localStorage.setItem('promoCache', JSON.stringify(data.data));
+                        localStorage.setItem('promoCacheExpiry', (now + 6 * 60 * 60 * 1000).toString());
+                    } else {
+                        console.log('Loaded from cache');
+                        setBranchs(parsedCache);
+                    }
+                } else {
+                    console.log('Fetched from API');
+                    setBranchs(data.data);
+                    localStorage.setItem('promoCache', JSON.stringify(data.data));
+                    localStorage.setItem('promoCacheExpiry', (now + 6 * 60 * 60 * 1000).toString());
+                }
+            } else {
+                console.error('Invalid response data format:', data);
+            }
+        } catch (error) {
+            console.error('Error fetching banners:', error);
+            if (cachedData) {
+                setBranchs(JSON.parse(cachedData));
+                console.log('Loaded from cache after API error');
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchData();
+  }, [baseUrl]);
+
+if (loading) {
+    return (
+        <>
+            <div className={loadingStyles.box}>
+                <div className={loadingStyles.content}>
+                    <img src="../images/logo.svg"/>
+                    <span>Loading</span>
+                </div>
+            </div>
+        </>
+    );
+}
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,41 +149,41 @@ export default function KebijakanPrivasi() {
   }, [baseUrl]);
 
   const schemaData = {
-      "@context": "https://schema.org",
-      "@type": "WebPage",
-      name: `Kebijakan Privasi - NMW Aesthetic Clinic`,
-      description: `Temukan jawaban atas pertanyaan umum tentang layanan, perawatan, konsultasi, dan prosedur medis di NMW Aesthetic Clinic. Dapatkan informasi lengkap untuk perawatan kecantikan dan kesehatan kulit Anda.`,
-      url: `${mainUrl}/kebijakan-privasi`,
-      publisher: {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    name: `Cabang - NMW Aesthetic Clinic`,
+    description: `Alamat Cabang & Kantor NMW Aesthetic Clinic`,
+    url: `${mainUrl}/cabang`,
+    publisher: {
       "@type": "Organization",
       name: "NMW Aesthetic Clinic",
       logo: {
-          "@type": "ImageObject",
-          url: `${mainUrl}/images/kebijakan-privasi.png`
+        "@type": "ImageObject",
+        url: `${mainUrl}/images/cabang-banner.png`
       }
-      },
-      mainEntityOfPage: {
+    },
+    mainEntityOfPage: {
       "@type": "WebPage",
-      "@id": `${mainUrl}/kebijakan-privasi`
-      },
-      breadcrumb: {
-          "@type": "BreadcrumbList",
-          itemListElement: [
-              {
-              "@type": "ListItem",
-                  position: 1,
-                  name: "Home",
-                  item: `${mainUrl}`
-              },
-              {
-              "@type": "ListItem",
-              position: 2,
-                  name: "Kebijakan Privasi",
-                  item: `${mainUrl}/kebijakan-privasi`
-              }
-          ]
-      }
-  };
+      "@id": `${mainUrl}/cabang`
+    },
+    breadcrumb: {
+        "@type": "BreadcrumbList",
+        itemListElement: [
+            {
+            "@type": "ListItem",
+                position: 1,
+                name: "Home",
+                item: `${mainUrl}`
+            },
+            {
+            "@type": "ListItem",
+                position: 2,
+                name: "Cabang",
+                item: `${mainUrl}/cabang`
+            }
+        ]
+    }
+};
 
   return (
     <>
