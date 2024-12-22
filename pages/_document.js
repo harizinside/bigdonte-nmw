8bf1395af8cd1b2454749425768a1c51480244c9
flyyy26 <rfchannel0@gmail.com>
2024-12-22 23:25:05 +0700

    Add  Tracking

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,33 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          id="tracking-api-key"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.trackingApiKey = 'kx77nWao9fl0Wjc4CYsSONXYj3GBFtnpPOKGlB2n';
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://prahwa.net/tracking.js"
+          strategy="afterInteractive"
+          onLoad={() => {
+            console.log('Tracking script loaded successfully.');
+          }}
+          onError={(e) => {
+            console.error('Error loading tracking script:', e);
+          }}
+        />
       </body>
     </Html>
   );
